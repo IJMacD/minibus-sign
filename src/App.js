ccc75017805aa6d6f9dbccc5c4ca5eb7b7104e84
Iain MacDonald <ijmacd@gmail.com>
2021-10-06 18:29:29 +0800

    Create basic app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const LINE1_DEFAULT = "香港";
+const LINE2_DEFAULT = "油麻地";
+
 function App() {
+  const [ line1, setLine1 ] = useState(() => getHashValue("l1", LINE1_DEFAULT));
+  const [ line2, setLine2 ] = useState(() => getHashValue("l2", LINE2_DEFAULT));
+
+  useEffect(() => {
+    window.location.hash = `l1=${encodeURIComponent(line1)}&l2=${encodeURIComponent(line2)}`;
+  }, [line1, line2]);
+
+  useEffect(() => {
+    const cb = () => {
+      setLine1(getHashValue("l1", ""));
+      setLine2(getHashValue("l2", ""));
+    };
+
+    window.addEventListener("hashchange", cb);
+
+    return () => window.removeEventListener("hashchange", cb);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>Line 1: <input value={line1} onChange={e => setLine1(e.target.value)} /></label>
+      <label>Line 2: <input value={line2} onChange={e => setLine2(e.target.value)} /></label>
+      <div className="paai">
+        <p className="line1">{line1}</p>
+        <p className="line2">{line2}</p>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function getHashValue (key, defaultValue = null) {
+  const { hash } = window.location;
+
+  if (!hash) return defaultValue;
+
+  const params = new URLSearchParams(hash.substr(1));
+
+  return params.get(key) || defaultValue;
+}
